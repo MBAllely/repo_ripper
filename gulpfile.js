573e89174ba84b2d3fee95493c91cd367272302a
Marika <mballely@gmail.com>
2016-03-18 08:34:58 -0700

    add index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var browserfiy = require('browserfiy');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 
-gulp.task('jsBrowserify', function() {
-  return browserfiy({ entries: ['./js/browser.js' ]})
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('concatInterface', function() {
-  return gulp.src(['./js/browser.js', './js/github-interface.js'])
+  return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
