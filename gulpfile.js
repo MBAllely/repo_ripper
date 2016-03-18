b3841360361c2365d90a431656a65dde19f37dd0
Marika <mballely@gmail.com>
2016-03-18 08:26:36 -0700

    install gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var browserfiy = require('browserfiy');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('jsBrowserify', function() {
   return browserfiy({ entries: ['./js/browser.js' ]})
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/browser.js', './js/github-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
 });
