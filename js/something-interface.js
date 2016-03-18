183a9cd358f9ef4e6b4d108e35de60b4f861d448
Marika <mballely@gmail.com>
2016-03-18 08:14:15 -0700

    initial commit

---
+++ js/something-interface.js
@@ -0,0 +1,9 @@
+var apiKey = cf52430fefa2646359b8b026e12859865d9e56cf;
+
+exports.getRepos = function(){
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
