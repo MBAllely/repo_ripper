96d75101b3206d00cb5cbbfb5c8b158ba0c70db2
Marika <mballely@gmail.com>
2016-03-18 09:57:02 -0700

    add skeletal functionality to the page

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,7 +1,13 @@
+var apiKey = require('./../.env').apiKey;
 var username = "";
 
 $(document).ready(function() {
   $('#getUserInfo').click(function() {
     username = $('#user_name').val();
-  })
-})
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  });
+});
